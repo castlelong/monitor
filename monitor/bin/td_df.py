c8532aabe1cdca156a6cfaae4affc26a8b0a4218
castle long <12804193@qq.com>
2017-12-29 15:22:46 +0800

    更改查询代付方式，将查询时间模式变为查询ID模式 并改写代码为面向编程方式

--- monitor/bin/td_df.py
+++ monitor/bin/td_df.py
@@ -8,11 +8,22 @@
 import sys
 import os
 import time
+import logging
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # print(base_dir)
 sys.path.append(base_dir)
-from bin import conn
-from bin import falcon
+from bin import falcon_oop
+from conf import dbconfig
+from conf import mysql_conn
+logging.basicConfig(filename=base_dir + 'df.log', level=logging.INFO,\
+                    format='%(asctime)s %(message)s', datefmt='%Y/%m/%d %H:%M:%S')
+
+
+def mysql(sql):
+    pro_config = dbconfig.al_pro_config()
+    al_conn = mysql_conn.DbConnect(pro_config, sql, '代付模块')
+    re = al_conn.select()
+    return re
 
 
 def run():
@@ -20,20 +31,28 @@ def run():
     2分钟查询失败笔数
     :return:
     """
-    start_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-    start_time = int(time.mktime(time.strptime(start_time, '%Y-%m-%d %H:%M:%S')))
+    id_sql = 'select max(id) from  posp_route.t_trade_flow'
+    result = mysql(id_sql)
+    start_id = result[0][0]
+    # print(start_id)
     while True:
-        time.sleep(120)
-        end_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-        end_time = int(time.mktime(time.strptime(end_time, '%Y-%m-%d %H:%M:%S')))
-        sql = """select count(*) from  posp_route.t_trade_flow where unix_timestamp(t_trade_flow.TRADE_TIME)> %s \
-               and unix_timestamp(t_trade_flow.TRADE_TIME)< %s and  state='2'""" % (start_time, end_time)
-        # print(sql)
-        # exit()
-        result = conn.f_trade(sql)
-        falcon.df('failed', result[0])
-        start_time = end_time
+        try:
+            time.sleep(120)
+            result = mysql(id_sql)
+            end_id = result[0][0]
+            # print(end_id)
+            sql = """select count(*) from  posp_route.t_trade_flow where id>= %s \
+                   and id<= %s and  state='2'""" % (start_id, end_id)
+            result = mysql(sql)
+            logging.info('代付失败数量：%s' % result[0][0])
+            update = falcon_oop.FalconMain("DF", "DFfailed", result[0][0], 60, "DF Error Type")
+            re_update = update.td()
+            logging.info("代付失败数据上传Falcon：%s" % re_update)
+            start_id = end_id
+        except Exception as e:
+            logging.error('代付失败数量查询错误：%s' % e)
         # print('td_df')
-
+#
+#
 # run()
 
